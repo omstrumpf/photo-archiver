f264cf48b1a1cefd9200d9fd39355c71963feec4
omstrumpf <omstrumpf@gmail.com>
2021-12-30 11:19:43 -0500

    parsing creation time from photos api

--- lib/google_photos/types.ml
+++ lib/google_photos/types.ml
@@ -1,5 +1,10 @@
 open! Core
 
+module Media_metadata = struct
+  type t = { creation_time : string [@jojson.option] [@key "creationTime"] }
+  [@@deriving sexp, yojson] [@@yojson.allow_extra_fields]
+end
+
 module Media_item = struct
   type t = {
     id : string;
@@ -8,6 +13,8 @@ module Media_item = struct
     product_url : string option; [@yojson.option] [@key "productURL"]
     base_url : string option; [@yojson.option] [@key "baseURL"]
     mime_type : string option; [@yojson.option] [@key "mimeType"]
+    media_metadata : Media_metadata.t option;
+        [@yojson.option] [@key "mediaMetadata"]
   }
   [@@deriving sexp, yojson] [@@yojson.allow_extra_fields]
 end
--- lib/google_photos/types.mli
+++ lib/google_photos/types.mli
@@ -1,5 +1,9 @@
 open! Core
 
+module Media_metadata : sig
+  type t = { creation_time : string } [@@deriving sexp, yojson]
+end
+
 module Media_item : sig
   type t = {
     id : string;
@@ -8,6 +12,7 @@ module Media_item : sig
     product_url : string option;
     base_url : string option;
     mime_type : string option;
+    media_metadata : Media_metadata.t option;
   }
   [@@deriving sexp, yojson]
 end
